e871ff77fd1c317d22b76d823c03c6d559ffedd1
Babatunde <babatundea15@gmail.com>
2020-07-28 12:40:17 +0100

    added a second method

--- add_two_numbers/add_two_numbers.py
+++ add_two_numbers/add_two_numbers.py
@@ -10,5 +10,17 @@
  * @param {ListNode} l2
  * @return {ListNode}
 """
+
+
+class Node():
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+
 def addTwoNumbers(l1, l2):
+    # Do a for-loop
+    # Traverse the list from the end
+    # Concatenate the integer together
+    # If the length of the integer and the linked list is the same, add with l2
     pass
--- contains-dupes/contains_dupe.js
+++ contains-dupes/contains_dupe.js
@@ -1,29 +1,27 @@
-// Method 1 
+// Method 1
 let array1 = [1, 2, 3, 4, 5, 2, 1, 1, 5];
 
-// function checkDuplicates(array) {
-//   let itemsAlreadySeen = [];
-//   for (let index = 0; index < array.length; index++) {
-//     const element = array[index];
-//     // The indexOf method returns the position of the first occurrence. It returns -1 if there is no such value
-//     if (itemsAlreadySeen.indexOf(element) !== -1) {
-//       return true;
-//     } else {
-//       itemsAlreadySeen.push(element);
-//       return false;
-//     }
-//   }
-// }
+function checkDuplicates(array) {
+  let itemsAlreadySeen = [];
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    // The indexOf method returns the position of the first occurrence. It returns -1 if there is no such value
+    if (itemsAlreadySeen.indexOf(element) !== -1) {
+      return true;
+    } else {
+      itemsAlreadySeen.push(element);
+      return false;
+    }
+  }
+}
 
-// checkDuplicates(array1);
-
+checkDuplicates(array1);
 
 // Method 2: This is a simpler and shorter method.
 
 function checkDuplicate(array) {
   // We use set since it does not permit duplicates
   if (new Set(array).size !== array.length) {
-    
     return true;
   }
   return false;
